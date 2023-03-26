f6ed9e62d4829247eb5359a0b569970e3e53c814
Diego <d.2120leandro@gmail.com>
2023-03-26 00:48:43 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,23 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'pizza com queijo mussarela derretido por cima da massa e molho de tomate.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'pizza com fatias de linguiça calabresa, queijo mussarela e molho de tomate.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'pizza com uma mistura de queijos, geralmente mussarela, provolone, parmesão e gorgonzola, além de molho de tomate.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'pizza com presunto, queijo mussarela, molho de tomate e fatias de tomate fresco.'},
+    {id:5, name:'Bahiana', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'pizza com molho de tomate, queijo mussarela, camarão refogado, pimenta dedo-de-moça e coentro.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:"pizza doce com uma camada de chocolate derretido sobre a massa e mm's. "}
+];
+
+let italianFood = [
+    {id:1, name:'Ravioli de camarão c/ chutney', img:'images/ravioli.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Uma combinação exótica e deliciosa de camarões suculentos, envoltos em massa de ravioli, cobertos com um molho rico de tomate e pesto, e servidos com uma pitada de chutney de manga adocicado.'},
+    {id:2, name:'Raviolone de gema', img:'images/raviolone.webp', price:18.00, sizes:['320g', '530g', '860g'], description:'Um prato de entrada que é uma explosão de sabores e texturas - raviolone recheado com gema de ovo caipira, ricota e espinafre, regado com manteiga, sálvia e trufas para um sabor luxuoso.'},
+    {id:3, name:'Fettuccine com camarão', img:'images/fettuccinecamarao.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Este prato de massa fettuccine é servido com camarões tenros e suculentos, envoltos em um molho rico de tomate, com brócolis crocantes e azeitonas para adicionar um sabor extra.'},
+    {id:4, name:'Nhoque com camarão e abobrinhas grelhadas', img:'images/nhoquecamarao.png', price:19.77, sizes:['320g', '530g', '860g'], description:'O nhoque macio é coberto com camarões e abobrinhas grelhadas, regados com um molho de tomate caseiro e finalizado com manjericão fresco para um sabor delicioso.'},
+    {id:5, name:'Filé mignon com gorgonzola', img:'images/filemignongorgonzola.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Este prato de carne é uma verdadeira festa para os sentidos, com um bife de filé mignon grelhado e coberto com queijo gorgonzola derretido, acompanhado de um molho cremoso de funghi e um risoto de parmesão delicioso.'},
+    {id:6, name:'Paillard (bife de filé fininho)', img:'images/paillard.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Um bife de filé fininho, temperado com ervas e grelhado para perfeição, regado com azeite de ervas e servido com um molho rico de madeira.'},
+    {id:6, name:'Nhoque ao molho de camarão e funghi', img:'images/nhoquecamaraofunghi.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Este prato de nhoque é coberto com um molho cremoso de camarão e funghi, proporcionando um sabor delicioso e aconchegante.'},
+    {id:6, name:'Risoto de camarão com tomate fresco, rúcula e tabasco:', img:'images/risotocamarao.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Um risoto cremoso e saboroso de camarão, com tomates frescos, rúcula picante e um toque de tabasco para um sabor extra.'},
+    {id:6, name:'Ravioli de queijo brie com damasco ao creme de gorgonzola:', img:'images/raviolibrie.jpg', price:18.55, sizes:['320g', '530g', '860g'], description:'Uma combinação única de queijo brie derretido, damascos adocicados e creme de gorgonzola, envolvido em massa de ravioli fresca para criar um prato rico e delicioso.'}
+   
+]
+
